ff1a985f1a9e7ab69ca128aa0077ee988bc0b369
joyunju <whdbswn1113@naver.com>
2022-04-13 15:53:11 +0900

    char

--- src/com/javaex/ex01/Ex04.java
+++ src/com/javaex/ex01/Ex04.java
@@ -5,5 +5,23 @@ package com.javaex.ex01;
 public class Ex04 {
 	public static void main(String[] args) {	
 		
+		char ch01 = 'A';
+		char ch02 = '쭈';
+		
+		System.out.println(ch01);
+		System.out.println(ch02);
+		
+		//문자형이기 때문에 숫자를 넣으면 값이 나오지 않음 
+		char ch03 = 65;
+		System.out.println(ch03);
+		
+		//문자형 자료형을 사용하는데 숫자값 보여주고 싶을 때
+		char ch04 = '3';
+		System.out.println(ch04);
+		
+		//문자열 작성시 string
+		//String은 무조건 " " 쌍따옴표 안에 작성해야함 
+		String str = "안녕하세요";
+		System.out.println(str);
 	}
 }
