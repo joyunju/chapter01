c380ef1f889aba29abd29cdee70cd171eb810c2b
joyunju <whdbswn1113@naver.com>
2022-04-14 10:49:22 +0900

    상수(Constant)

--- src/com/javaex/ex01/Ex05.java
+++ src/com/javaex/ex01/Ex05.java
@@ -1,20 +1,41 @@
-//변수와 자료형 연습 문제 
+//상수(Constant)
 package com.javaex.ex01;
 
 public class Ex05 {
 	public static void main(String[] args) {
 		
-//		(String ) number; // 학번
-//		(String ) name; // 이름
-//		(boolean ) isEnrolled //등록 여부 
-//		(float ) grade; // 평점
-//		(String ) address; // 주소
-//		(String ) major; // 전공
-//		(int ) unit; // 이수 학점
-//		(boolean ) haveMinor; // 부전공 여부
-//		(String ) juminNo; // 주민번호 (-없이 13자리숫자)
-//		(String ) cellphone; // 핸드폰 번호 (-포함한 숫자)
-//		(int ) age; // 나이
-//		(String ) email; // 이메일주소
+		//일반적인 방식
+		double pi = 3.14;
+		//결과 값이 소수점으로 나올 예정이니까 double
+		double result01 = pi*5*5;
+		
+		System.out.println(result01);
+		
+		//pi값이 변경되는 경우
+		pi = 3.1415926;
+		double result02 = pi*5*5;
+		
+		System.out.println(result02);
+		
+		//보통 변수의 이름을 소문자로 적지만 변경될 수 없는 고정하고 싶은 데이터는 대문자로 작성한다.
+		//상수로 정의할 경우
+		// ex) final double PI = 3.14;
+		
+		final double PI = 3.14;
+		double result03 = PI*5*5;
+		System.out.println(result03);
+		
+		//상수의 값을 변경할려고 할 때
+		// 아래는 오류가 나는 것이 맞음
+		// 위에서 PI의 값을 고정시켜서 바꿀 수 없음
+		/*
+		PI = 3.1415926;
+		double result04 = PI*5*5;
+		System.out.println(result04);
+		*/
+		
+		
+		
+		
 	}
 }
