683e7ca805dcf925609cbf5bcbc0901be72ddb5c
joyunju <whdbswn1113@naver.com>
2022-04-14 14:55:13 +0900

    증가, 감소 연산자

--- src/com/javaex/ex01/Ex08.java
+++ src/com/javaex/ex01/Ex08.java
@@ -8,33 +8,54 @@ public class Ex08 {
 		// 대입연산자
 		int a = 7;
 		int b = 2;
-		
+
 		System.out.println("산술연산자");
 		// 산술연산자
-		System.out.println(a+b); //결과값 9
-		System.out.println(a-b); //결과값 5
-		System.out.println(a*b); //결과값 14
-		System.out.println(a/b); //결과값 3
-		System.out.println(a%b); //결과값 1 --> 7나누기 2 = 3.1... --> 나머지 값을 반환하니까
-		
+		System.out.println(a + b); // 결과값 9
+		System.out.println(a - b); // 결과값 5
+		System.out.println(a * b); // 결과값 14
+		System.out.println(a / b); // 결과값 3
+		System.out.println(a % b); // 결과값 1 --> 7나누기 2 = 3.1... --> 나머지 값을 반환하니까
+
 		// 산술 연산자 % 자세히
 		System.out.println("산술연산자 / % 자세히");
 		System.out.println("===========");
-		System.out.println(7/2);       //결과값 3
-		System.out.println(7.0/2.0);   //결과값 3.5
-		System.out.println(7%2);       //결과값 1
-		System.out.println(7.0%2.0);   //결과값 1.0
+		System.out.println(7 / 2); // 결과값 3
+		System.out.println(7.0 / 2.0); // 결과값 3.5
+		System.out.println(7 % 2); // 결과값 1
+		System.out.println(7.0 % 2.0); // 결과값 1.0
 		System.out.println("========");
-		
-		//부호 연산자
+
+		// 부호 연산자
 		int var = -3;
-		int pVar = +var; //현재 var 안에 있는 부호 그대로
+		int pVar = +var; // 현재 var 안에 있는 부호 그대로
 		int mVar = -var; // 현재 var 안에 있는 부호 반대로
+
+		System.out.println(var); // 결과값 3
+		System.out.println(pVar); // 결과값 -3
+		System.out.println(mVar); // 결과값 3
+
+		// 증가, 감소 연산자
+		System.out.println("=====증가연산자=====");
+		// 증가, 감소 연산자를 앞에다 쓰면 증가한 후의 값을 대입하고 실제 값이 바뀐
+		int num1 = 7;
+
+		System.out.println(num1);    //결과값 7
+		System.out.println(++num1);  // 결과값 8
+		System.out.println(num1);    //결과값 8
+		System.out.println(num1++);  //결과값 8
+		System.out.println(num1);    //결과값 9
 		
-		System.out.println(var);  //결과값 3
-		System.out.println(pVar); //결과값 -3 
-		System.out.println(mVar);  //결과값 3
-		
+		System.out.println("=====감소연산자=====");
+		// 뒤에다가 쓰면 증가하기 전에 먼저 값을 대입하고 후에 증가
+		int num2 = 2;
+
+		System.out.println(num2);    //결과값 2
+		System.out.println(--num2);  // 결과값 1
+		System.out.println(num2);    //결과값 1
+		System.out.println(num2--);  //결과값 1
+		System.out.println(num2);    //결과값 0
+		System.out.println(--num2);  //결과값 -1
 
 	}
 }
