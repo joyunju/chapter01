f18c70e229831408bb68dbe0c518540410de64c5
joyunju <whdbswn1113@naver.com>
2022-04-19 15:17:22 +0900

    continue 없이 출력 방법2

--- src/com/javaex/ex04/Ex11.java
+++ src/com/javaex/ex04/Ex11.java
@@ -5,12 +5,31 @@ package com.javaex.ex04;
 
 public class Ex11 {
 	public static void main(String[] args) {
-		
-		for(int i=1; i<=20; i++) {
-			if(i%2==0 || i%3==0) {
+
+		for (int i = 1; i <= 20; i++) {
+			if (i % 2 == 0 || i % 3 == 0) {
 				continue;
 			}
 			System.out.println(i);
+		}
+
+		System.out.println("======continue 없이 출력 방법=======");
+
+		for (int i = 1; i <= 20; i++) {
+			if (i % 2 == 0 || i % 3 == 0) {
+				// 2의 배수이면서 3의 배수는 출력하지 말기 위해서 비워두고
+			} else {
+				System.out.println(i);
+			}
+		}
+
+		System.out.println("======continue 없이 출력 방법2=======");
+
+		for (int i = 1; i <= 20; i++) {
+			if (!(i % 2 == 0 || i % 3 == 0)) {
+			// ! --> 는 조건값을 반대로 만들어줌  --> true를 false로 바꿔줌 
+				System.out.println(i);
+			}
 		}
 	}
 
