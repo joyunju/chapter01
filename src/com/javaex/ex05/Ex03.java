671e41973f4ab9211a143d130f6e48e328c114e3
joyunju <whdbswn1113@naver.com>
2022-04-20 11:55:17 +0900

    배열 선언과 생성 -> 선언과 함께생성 : 로또

---
+++ src/com/javaex/ex05/Ex03.java
@@ -0,0 +1,36 @@
+//타입[] 변수명 = new 타입[크기]
+
+package com.javaex.ex05;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		//선언과 함께 생성
+//		int[] intArray //선언
+//		intArray = new int[6]; //생성 
+		
+		int[] nums = new int[6];
+
+//		nums[0] = (int) (Math.random() * 45) + 1;
+//		nums[1] = (int) (Math.random() * 45) + 1;
+//		nums[2] = (int) (Math.random() * 45) + 1;
+//		nums[3] = (int) (Math.random() * 45) + 1;
+//		nums[4] = (int) (Math.random() * 45) + 1;
+//		nums[5] = (int) (Math.random() * 45) + 1;
+		
+		System.out.println("=====for문으로 출력====");
+		for(int i=0; i<6; i++) {
+			nums[i] = (int) (Math.random() * 45) + 1;
+		}
+		
+
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+
+		System.out.println("=====for문으로 출력====");
+		// int i =0 --> nums 의 방번호가 0 부터 시작이니까
+		for (int i = 0; i < 6; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+}
